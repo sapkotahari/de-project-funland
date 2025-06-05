dcea76532f093555643ee014f409c86f1614965e
sapkotahari <hari.sapkota1@gmail.com>
2025-06-05 15:28:48 +0100

    create transform.py file and start writing docstrings for util functions

---
+++ src/transform.py
@@ -0,0 +1,14 @@
+
+
+### template for util functions related to transform_lambda_handler
+
+def transform_lambda_handler(event, context):
+    """_summary_
+    write a lambda handler for transformation step in the ETL pipeline.
+
+    This lambda function will be triggered by new data being uploaded into S3_ingestion_bucket.
+    
+    """
+    
+    pass
+
